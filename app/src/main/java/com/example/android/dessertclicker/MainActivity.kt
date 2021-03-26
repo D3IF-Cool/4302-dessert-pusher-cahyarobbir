818fc8d29eca3a601ee76f6890ce183f791a8ebc
cahyarobbiramadhan <cahyarobbi@student.telkomuniversity.ac.id>
2021-03-26 19:18:39 +0700

    Menerapkan Lifecycle Observer

--- app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -73,7 +73,7 @@ class MainActivity : AppCompatActivity() {
         binding.dessertButton.setOnClickListener {
             onDessertClicked()
         }
-        dessertTimer = DessertTimer()
+        dessertTimer = DessertTimer(this.lifecycle)
 
         // Set the TextViews to the right values
         binding.revenue = revenue
@@ -152,7 +152,7 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         Timber.i( "onStart Called")
-        dessertTimer.startTimer()
+        
     }
     override fun onResume() {
         super.onResume()
@@ -165,7 +165,7 @@ class MainActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         Timber.i("onStop Called")
-        dessertTimer.stopTimer()
+
     }
     override fun onDestroy() {
         super.onDestroy()
